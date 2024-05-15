e397c6148591bcb131043e903781637f4b0cf5cc
moaberglund <moa.21@hotmail.com>
2024-05-15 15:40:47 +0200

    processa fetch och skriv ut meny

--- src/js/menu.js
+++ src/js/menu.js
@@ -7,6 +7,7 @@ function init() {
     fetchAPI();
     processAPI();
 }
+window.onload = init();
 
 //hämta data från API
 async function fetchAPI() {
@@ -22,3 +23,87 @@ async function fetchAPI() {
     }
 }
 
+async function processAPI() {
+    const result = await fetchAPI();
+    if (!result) {
+        return //avbryt
+    }
+
+    //utskrift DOM
+
+    //article tagg för nibble
+    const nibble = document.getElementById("nibble");
+    nibble.innerHTML = "";
+    //article tagg för pizzas
+    const pizza = document.getElementById("pizza");
+    pizza.innerHTML = "";
+    //article för efterrätter
+    const sweet = document.getElementById("sweet");
+    sweet.innerHTML = "";
+    //article för vin
+    const wine = document.getElementById("wine");
+    wine.innerHTML = "";
+
+    result.forEach(item => {
+        const id = item._id;
+        const category = item.category;
+        const name = item.name;
+        const price = item.price;
+        const description = item.description;
+
+        //skapa div per menyobject
+        //NIBBLE
+        const nibbleDiv = document.createElement("div");
+        nibbleDiv.classList.add("nibble-object");
+        //PIZZA
+        const pizzaDiv = document.createElement("div");
+        pizzaDiv.classList.add("pizza-object");
+        //SWEET
+        const sweetDiv = document.createElement("div");
+        sweetDiv.classList.add("sweet-object")
+        //WINE
+        const wineDiv = document.createElement("div");
+        wineDiv.classList.add("wine-object");
+
+        //om kategori är nibble
+        if (category === "Nibble") {
+            nibbleDiv.innerHTML = `
+        <h3>${name}</h3>
+        <p>${price} &euro; </p>
+        <p>${description}</p>
+        `
+        nibble.appendChild(nibbleDiv);
+        }
+
+        //om kategori är pizza
+        if (category === "Pizza") {
+            pizzaDiv.innerHTML = `
+        <h3>${name}</h3>
+        <p>${price} &euro; </p>
+        <p>${description}</p>
+        `
+        pizza.appendChild(pizzaDiv);
+        }
+
+        //om kategori är efterrätt
+        if (category === "Sweet") {
+            sweetDiv.innerHTML = `
+        <h3>${name}</h3>
+        <p>${price} &euro; </p>
+        <p>${description}</p>
+        `
+        sweet.appendChild(sweetDiv);
+        }
+
+        //om kategori är vin
+        if (category === "Wine") {
+            wineDiv.innerHTML = `
+        <h3>${name}</h3>
+        <p>${price} &euro;</p>
+        <p>${description}</p>
+        `
+        wine.appendChild(wineDiv);
+        }
+    })
+
+}
